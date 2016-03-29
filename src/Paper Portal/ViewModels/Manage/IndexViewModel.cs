860bdb447e0a8be939339a09dd1ba5b89901d5e8
Aadam <aadimator@gmail.com>
2016-03-28 21:15:00 -0700

    Removed Unnecessary things from ManageController

--- src/Paper Portal/ViewModels/Manage/IndexViewModel.cs
+++ src/Paper Portal/ViewModels/Manage/IndexViewModel.cs
@@ -9,13 +9,5 @@ namespace Paper_Portal.ViewModels.Manage
     public class IndexViewModel
     {
         public bool HasPassword { get; set; }
-
-        public IList<UserLoginInfo> Logins { get; set; }
-
-        public string PhoneNumber { get; set; }
-
-        public bool TwoFactor { get; set; }
-
-        public bool BrowserRemembered { get; set; }
     }
 }
