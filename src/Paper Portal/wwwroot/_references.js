f53dbbd71614d77c41bd5278946b0a538c38bb23
Aadam <aadimator@gmail.com>
2016-03-30 05:50:38 -0700

    Material Design Initial

--- src/Paper Portal/wwwroot/_references.js
+++ src/Paper Portal/wwwroot/_references.js
@@ -1,5 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
+/// <reference path="js/material.js" />
 /// <reference path="js/materialize.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
